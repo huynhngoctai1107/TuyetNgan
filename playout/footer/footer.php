8cb821ad1b334b13243d24af84983758e3def869
tuyetngan1707 <tuyetngan170702.z@gmail.com>
2022-11-25 13:30:12 +0700

    A

--- playout/footer/footer.php
+++ playout/footer/footer.php
@@ -11,20 +11,19 @@
                                 <div class="single-widget">
                                     <h2>Dịch Vụ</h2>
                                     <ul class="nav nav-pills nav-stacked">
-                                        <li><a href="404.php">Trợ giúp trực tuyến</a></li>
-                                        <li><a href="404.php">Liên hệ chúng tôi</a></li>
-                                        <li><a href="404.php">Câu hỏi thường gặp</a></li>
+                                        <li><a href="contact-us.php">Liên hệ chúng tôi</a></li>
+                                        <li><a href="">Câu hỏi thường gặp</a></li>
                                     </ul>
                                 </div>
                             </div>
                             <div class="col-sm-2">
                                 <div class="single-widget">
-                                    <h2>Quock Shop</h2>
+                                    <h2>SẢN PHẨM</h2>
                                     <ul class="nav nav-pills nav-stacked">
-                                        <li><a href="404.php">Áo Thun</a></li>
-                                        <li><a href="404.php">Nam</a></li>
-                                        <li><a href="404.php">Phụ Nữ</a></li>
-                                        <li><a href="404.php">Giày</a></li>
+                                        <li><a href="index.php">Các loại bánh</a></li>
+                                        <li><a href="index.php">Bánh ngọt</a></li>
+                                        <li><a href="index.php">Bánh kem</a></li>
+                                        <li><a href="index.php">Bánh bơ</a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -41,22 +40,22 @@
                             </div>
                             <div class="col-sm-2">
                                 <div class="single-widget">
-                                    <h2>GIỚI THIỆU VỀ SHOPPER</h2>
+                                    <h2>GIỚI THIỆU</h2>
                                     <ul class="nav nav-pills nav-stacked">
-                                        <li><a href="404.php">Thông Tin Cửa Hàng</a></li>
-                                        <li><a href="404.php">Vị Trí Cửa Hàng</a></li>
+                                        <li><a href="gioithieu.php">Thông Tin Cửa Hàng</a></li>
+                                        <li><a href="contact-us.php">Vị Trí Cửa Hàng</a></li>
                                         <li><a href="404.php">Chương Trình Liên Kết</a></li>
-                                        <li><a href="404.php">Bản Quyền</a></li>
+                                      <li><a href="404.php">Bản Quyền</a></li>
                                     </ul>
                                 </div>
                             </div>
                             <div class="col-sm-3 col-sm-offset-1">
                                 <div class="single-widget">
-                                    <h2>GIỚI THIỆU VỀ SHOPPER</h2>
+                                    <h2>TUYẾT NGÂN CAKE</h2>
                                     <form action="404.php" class="searchform">
                                         <input type="text" placeholder="Địa Chỉ Email Của Bạn" />
                                         <button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
-                                        <p>Nhận các bản cập nhật gần đây nhất từ ​​trang web của chúng tôi và được cập nhật bản thân của bạn ...</p>
+                                        <p>Những bánh ngon được cập nhật mỗi ngày tại cửa chân đến cửa hàng để mua nào. Hoan nghênh các bạn! ...</p>
                                     </form>
                                 </div>
                             </div>
@@ -68,7 +67,7 @@
                 <div class="footer-bottom">
                     <div class="container">
                         <div class="row" style="width: 100%; display: flex ; justify-content: space-between;  padding-top: 20px;">
-                            <p class="pull-left">Bản Quyền © 2020 E-SHOPPER Inc. Mọi quyền được bảo lưu..</p>
+                            <p class="pull-left">Bản Quyền © 2022 TUYẾT NGÂN CAKE Inc. Mọi quyền được bảo lưu..</p>
                             <p class="pull-right">Thiết kế bởi <span><a target="_blank" href="https://www.facebook.com/tai.huynhngoc.58152/">Huỳnh Ngọc Tài</a></span></p>
                         </div>
                     </div>
